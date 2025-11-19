52173dde18202dd6d5e849e2aaf184a864486394
Ali Mammadov <amammadov097@gmail.com>
2025-11-19 18:40:09 +0100

    preparing for the production

--- frontend/services/config.ts
+++ frontend/services/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://ps7c2bnr-7777.euw.devtunnels.ms/";
+export const API_URL = "https://quizvers.onrender.com/";
 // "http://192.168.178.46:7777/"; //Fritzbox
 // "http://192.168.68.104:7777/"; //pc Lotuz
 // "http://192.168.68.106:7777/"; //mac
--- frontend/utils/auth.tsx
+++ frontend/utils/auth.tsx
@@ -6,8 +6,6 @@ export const configureGoogleSignIn = () => {
       "748122014004-qsp2nu1g9s7dg12b5e2b19e9vfkbir8h.apps.googleusercontent.com",
     iosClientId:
       "748122014004-7r9fmi2vjal0vnvc4ft3bjcigcdedqtb.apps.googleusercontent.com",
-    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
     offlineAccess: true,
-    forceCodeForRefreshToken: true,
   });
 };
